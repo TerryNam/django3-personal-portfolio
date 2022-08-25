accfad487ce7f530324850a960038df252cb77b9
TerryNam <terrynam0502@gmail.com>
2022-08-25 19:36:40 +0000

    Remove old stuffs

--- personal_portfolio_project/settings.py
+++ personal_portfolio_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--j10bq5zj0+d+iflwn&$-v3ay5c5rh235y&s*ij0)6@@k#_rgy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['terrynam.pythonanywhere.com']
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
